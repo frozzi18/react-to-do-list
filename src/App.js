334f9b568ce7c5e3fe7aeb48e90e124817aceab8
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2021-03-01 17:35:28 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  // Variable list
+  const [list, setList] = useState([]);
+  const [inputList, setInputList] = useState("");
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setList([...list, inputList]);
+  };
+
+  function handleSubmitInside(todo) {
+    // event.preventDefault();
+    console.log(todo);
+    const newList = list.map((item) => {
+      if (item === todo) {
+        const updated_item = inputList;
+
+        return updated_item;
+      }
+
+      return item;
+    });
+    setList(newList);
+    setIsEdit(false);
+  }
+
+  useEffect(() => {
+    console.log(list);
+  }, [list]);
+
+  const handleChange = (event) => {
+    setInputList(event.target.value);
+  };
+
+  const handleDelete = (todo) => {
+    setList(list.filter((todo_) => todo_ !== todo));
+  };
+
+  function handleEdit(todo) {
+    setIsEdit(true);
+  }
+
+  const formFirst = () => {
+    if (!isEdit) {
+      return (
+        <form onSubmit={handleSubmit}>
+          <label>
+            <input
+              type="text"
+              name="todo_list"
+              value={inputList}
+              onChange={handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>TO DO LIST</h1>
+        {formFirst()}
+        <ul>
+          {list.map((todo) => {
+            return (
+              <>
+                <ol>
+                  {isEdit ? (
+                    <form onSubmit={() => handleSubmitInside(todo)}>
+                      <label>
+                        <input
+                          type="text"
+                          name="todo_list"
+                          value={inputList}
+                          onChange={handleChange}
+                        />
+                      </label>
+
+                      <input type="submit" value="Submit" />
+                    </form>
+                  ) : (
+                    todo
+                  )}{" "}
+                  <button type="button" onClick={() => handleDelete(todo)}>
+                    Delete
+                  </button>
+                  <button type="button" onClick={() => handleEdit(todo)}>
+                    Edit
+                  </button>
+                </ol>
+              </>
+            );
+          })}
+        </ul>
+        <div></div>
+        {inputList}
       </header>
     </div>
   );
